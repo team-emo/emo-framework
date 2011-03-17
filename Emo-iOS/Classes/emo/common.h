4beefdb65d12037bf5ee10fffd494d0cb848fb8f
Kota Iguchi <infosia.jp@gmail.com>
2011-03-17 00:20:33 +0000

    move source

---
+++ Emo-iOS/Classes/emo/common.h
@@ -0,0 +1,14 @@
+#define _UNICODE
+
+#define EMO_LOG_TAG "EmoFramework"
+
+#define SQUIRREL_VM_INITIAL_STACK_SIZE 1024
+#define SQUIRREL_MAIN_SCRIPT "emomain.nut"
+
+#define EMO_NO_ERROR    0x0000
+#define EMO_ERROR       0x0001
+
+#define ERR_SCRIPT_LOAD      0x0100
+#define ERR_SCRIPT_OPEN      0x0101
+#define ERR_SCRIPT_COMPILE   0x0102
+#define ERR_SCRIPT_CALL_ROOT 0x0103
