a400cf6228601da4f533d19e83d0a80779fd6f35
Kota Iguchi <infosia.jp@gmail.com>
2011-03-24 12:05:21 +0000

    refactor sources

--- Emo-Android/jni/emo_engine.cpp
+++ Emo-Android/jni/emo_engine.cpp
@@ -1,4 +1,6 @@
+#include <EGL/egl.h>
 #include <GLES/gl.h>
+
 #include <android/log.h>
 #include <android_native_app_glue.h>
 #include <android/window.h>
@@ -6,20 +8,21 @@
 #include <squirrel.h>
 #include <common.h>
 #include <main.h>
+#include <sqfunc.h>
 
 extern struct engine *g_engine;
 
-/* Log Info */
+/* Log INFO */
 void LOGI(const SQChar* msg) {
 	((void)__android_log_print(ANDROID_LOG_INFO,  EMO_LOG_TAG, msg));
 }
 
-/* Log Warning */
+/* Log WARN */
 void LOGW(const SQChar* msg) {
 	((void)__android_log_print(ANDROID_LOG_WARN,  EMO_LOG_TAG, msg));
 }
 
-/* Log Error */
+/* Log ERROR */
 void LOGE(const SQChar* msg) {
 	((void)__android_log_print(ANDROID_LOG_ERROR, EMO_LOG_TAG, msg));
 }
@@ -182,3 +185,203 @@ SQInteger emoRegisterSensors(HSQUIRRELVM v) {
     }
     return 0;
 }
+
+/*
+ * register class and functions for script
+ */
+void initScriptFunctions(struct engine* engine) {
+    register_class(engine->sqvm, SQUIRREL_RUNTIME_CLASS);
+    register_class(engine->sqvm, SQUIRREL_EVENT_CLASS);
+
+    register_class_func(engine->sqvm, SQUIRREL_RUNTIME_CLASS, "import", emoImportScript);
+    register_class_func(engine->sqvm, SQUIRREL_RUNTIME_CLASS, "setOptions", emoSetOptions);
+    register_class_func(engine->sqvm, SQUIRREL_EVENT_CLASS,   "registerSensors", emoRegisterSensors);
+}
+
+void engine_update_uptime(struct engine* engine) {
+	timeb eventTime;
+	ftime(&eventTime);
+
+	engine->uptime.time     = eventTime.time - engine->startTime.time;
+	engine->uptime.millitm  = eventTime.millitm;
+	engine->uptime.timezone = eventTime.timezone;
+	engine->uptime.dstflag  = eventTime.dstflag;
+}
+
+/**
+ * Initialize the framework
+ */
+void emo_init_engine(struct engine* engine) {
+
+    // initialize startup time
+    ftime(&engine->startTime);
+
+    // initialize uptime
+    engine_update_uptime(engine);
+
+    engine->sqvm = sq_open(SQUIRREL_VM_INITIAL_STACK_SIZE);
+    engine->lastError = EMO_NO_ERROR;
+
+    // disable drawframe callback to improve performance (default)
+    engine->enableOnDrawFrame = SQFalse;
+
+    // enable perspective hint to nicest (default)
+    engine->enablePerspectiveNicest = SQTrue;
+
+    // init Squirrel VM
+    initSQVM(engine->sqvm);
+
+    // register class and functions for script
+    initScriptFunctions(engine);
+
+    // load runtime and main script
+    loadScriptFromAsset(SQUIRREL_RUNTIME_SCRIPT);
+    loadScriptFromAsset(SQUIRREL_MAIN_SCRIPT);
+
+    // force fullscreen
+    emoUpdateOptions(OPT_WINDOW_FORCE_FULLSCREEN);
+
+    // call onLoad()
+    callSqFunction(engine->sqvm, "onLoad");
+
+}
+
+/*
+ * Initialize the display
+ */
+void emo_init_display(struct engine* engine) {
+
+    /* initialize OpenGL state */
+
+    if (engine->enablePerspectiveNicest) {
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    } else {
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
+    }
+
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_LIGHTING);
+    glDisable(GL_MULTISAMPLE);
+    glDisable(GL_DITHER);
+    glDisable(GL_COLOR_ARRAY);
+
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_TEXTURE_COORD_ARRAY);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glEnable(GL_VERTEX_ARRAY);
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glCullFace(GL_BACK);
+}
+
+/*
+ * Draw current frame
+ */
+void emo_draw_frame(struct engine* engine) {
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    if (engine->enableOnDrawFrame) {
+    	callSqFunction(engine->sqvm, "onDrawFrame");
+    }
+}
+
+/*
+ * Terminate the framework
+ */
+void emo_dispose_engine(struct engine* engine) {
+    callSqFunction(engine->sqvm, "onDispose");
+    sq_close(engine->sqvm);
+}
+
+/*
+ * Process motion event
+ */
+int32_t emo_event_motion(struct android_app* app, AInputEvent* event) {
+	struct engine* engine = (struct engine*)app->userData;
+	size_t pointerCount =  AMotionEvent_getPointerCount(event);
+
+	engine_update_uptime(engine);
+
+	for (size_t i = 0; i < pointerCount; i++) {
+		size_t pointerId = AMotionEvent_getPointerId(event, i);
+		size_t action = AMotionEvent_getAction(event) & AMOTION_EVENT_ACTION_MASK;
+		size_t pointerIndex = i;
+		
+		if (action == AMOTION_EVENT_ACTION_POINTER_DOWN || action == AMOTION_EVENT_ACTION_POINTER_UP) {
+			pointerIndex = (AMotionEvent_getAction(event) & AMOTION_EVENT_ACTION_POINTER_INDEX_MASK) >> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
+			pointerId = AMotionEvent_getPointerId(event, pointerIndex);
+		}
+
+		engine->touchEventParamCache[0] = pointerId;
+		engine->touchEventParamCache[1] = action;
+		engine->touchEventParamCache[2] = AMotionEvent_getX(event, pointerIndex);
+		engine->touchEventParamCache[3] = AMotionEvent_getY(event, pointerIndex);
+		engine->touchEventParamCache[4] = engine->uptime.time;
+		engine->touchEventParamCache[5] = engine->uptime.millitm;
+		engine->touchEventParamCache[6] = AInputEvent_getDeviceId(event);
+		engine->touchEventParamCache[7] = AInputEvent_getSource(event);
+		
+		if (callSqFunction_Bool_Floats(engine->sqvm, "onMotionEvent", engine->touchEventParamCache, MOTION_EVENT_PARAMS_SIZE, false)) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+/*
+ * Process key event
+ */
+int32_t emo_event_key(struct android_app* app, AInputEvent* event) {
+	struct engine* engine = (struct engine*)app->userData;
+
+	engine_update_uptime(engine);
+
+	engine->keyEventParamCache[0] = AKeyEvent_getAction(event);
+	engine->keyEventParamCache[1] = AKeyEvent_getKeyCode(event);
+	engine->keyEventParamCache[2] = AKeyEvent_getRepeatCount(event);
+	engine->keyEventParamCache[3] = AKeyEvent_getMetaState(event);
+	engine->keyEventParamCache[4] = engine->uptime.time;
+	engine->keyEventParamCache[5] = engine->uptime.millitm;
+	engine->keyEventParamCache[6] = AInputEvent_getDeviceId(event);
+	engine->keyEventParamCache[7] = AInputEvent_getSource(event);
+
+	if (callSqFunction_Bool_Floats(engine->sqvm, "onKeyEvent", engine->keyEventParamCache, KEY_EVENT_PARAMS_SIZE, false)) {
+		return 1;
+	}
+    return 0;
+}
+
+/*
+ * handle sensor event TODO
+ */
+void emo_event_sensors(struct engine* engine, ASensorEvent* event) {
+   LOGI("engine_handle_sensors: not implemented yet.");
+}
+
+/*
+ * Gained focus
+ */
+void emo_gained_focus(struct engine* engine) {
+	callSqFunction(engine->sqvm, "onGainedFocus");
+    engine->animating = 1;
+}
+
+/*
+ * Lost focus
+ */
+void emo_lost_focus(struct engine* engine) {
+	callSqFunction(engine->sqvm, "onLostFocus");
+    engine->animating = 0;
+}
+
+/**
+ * Command from main thread: the system is running low on memory.
+ * Try to reduce your memory use.
+ */
+void emo_low_memory(struct engine* engine) {
+	callSqFunction(engine->sqvm, "onLowMemory");
+}
+
--- Emo-Android/jni/main.cpp
+++ Emo-Android/jni/main.cpp
@@ -33,216 +33,16 @@ extern SQInteger emoRegisterSensors(HSQUIRRELVM v);
 extern void emoUpdateOptions(SQInteger value);
 extern SQBool loadScriptFromAsset(const char* fname);
 
-static void engine_update_uptime(struct engine* engine) {
-	timeb eventTime;
-	ftime(&eventTime);
-
-	engine->uptime.time     = eventTime.time - engine->startTime.time;
-	engine->uptime.millitm  = eventTime.millitm;
-	engine->uptime.timezone = eventTime.timezone;
-	engine->uptime.dstflag  = eventTime.dstflag;
-}
-
-/*
- * register class and functions for script
- */
-static void initScriptFunctions(struct engine* engine) {
-    register_class(engine->sqvm, SQUIRREL_RUNTIME_CLASS);
-    register_class(engine->sqvm, SQUIRREL_EVENT_CLASS);
-
-    register_class_func(engine->sqvm, SQUIRREL_RUNTIME_CLASS, "import", emoImportScript);
-    register_class_func(engine->sqvm, SQUIRREL_RUNTIME_CLASS, "setOptions", emoSetOptions);
-    register_class_func(engine->sqvm, SQUIRREL_EVENT_CLASS,   "registerSensors", emoRegisterSensors);
-}
-
-/**
- * Initialize the framework
- */
-void emo_init_engine(struct engine* engine) {
-
-    // initialize startup time
-    ftime(&engine->startTime);
-
-    // initialize uptime
-    engine_update_uptime(engine);
-
-    engine->sqvm = sq_open(SQUIRREL_VM_INITIAL_STACK_SIZE);
-    engine->lastError = EMO_NO_ERROR;
-
-    // disable drawframe callback to improve performance (default)
-    engine->enableOnDrawFrame = SQFalse;
-
-    // enable perspective hint to nicest (default)
-    engine->enablePerspectiveNicest = SQTrue;
-
-    // init Squirrel VM
-    initSQVM(engine->sqvm);
-
-    // register class and functions for script
-    initScriptFunctions(engine);
-
-    // load runtime and main script
-    loadScriptFromAsset(SQUIRREL_RUNTIME_SCRIPT);
-    loadScriptFromAsset(SQUIRREL_MAIN_SCRIPT);
-
-    // force fullscreen
-    emoUpdateOptions(OPT_WINDOW_FORCE_FULLSCREEN);
-
-    // call onLoad()
-    callSqFunction(engine->sqvm, "onLoad");
-
-}
-
-/*
- * Initialize the display
- */
-void emo_init_display(struct engine* engine) {
-
-    /* initialize OpenGL state */
-
-    if (engine->enablePerspectiveNicest) {
-        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-    } else {
-        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
-    }
-
-    glDisable(GL_DEPTH_TEST);
-    glDisable(GL_LIGHTING);
-    glDisable(GL_MULTISAMPLE);
-    glDisable(GL_DITHER);
-    glDisable(GL_COLOR_ARRAY);
-
-    glEnable(GL_TEXTURE_2D);
-    glEnable(GL_TEXTURE_COORD_ARRAY);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    glEnable(GL_VERTEX_ARRAY);
-    glEnable(GL_CULL_FACE);
-    glFrontFace(GL_CCW);
-    glCullFace(GL_BACK);
-}
-
-/*
- * Draw current frame
- */
-void emo_draw_frame(struct engine* engine) {
-    glClearColor(0, 0, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    if (engine->enableOnDrawFrame) {
-    	callSqFunction(engine->sqvm, "onDrawFrame");
-    }
-}
-
-/*
- * Terminate the framework
- */
-void emo_dispose_engine(struct engine* engine) {
-    callSqFunction(engine->sqvm, "onDispose");
-    sq_close(engine->sqvm);
-}
-
-/*
- * Process motion event
- */
-static int32_t emo_event_motion(struct android_app* app, AInputEvent* event) {
-	struct engine* engine = (struct engine*)app->userData;
-	size_t pointerCount =  AMotionEvent_getPointerCount(event);
-
-	engine_update_uptime(engine);
-
-	for (size_t i = 0; i < pointerCount; i++) {
-		size_t pointerId = AMotionEvent_getPointerId(event, i);
-		size_t action = AMotionEvent_getAction(event) & AMOTION_EVENT_ACTION_MASK;
-		size_t pointerIndex = i;
-		
-		if (action == AMOTION_EVENT_ACTION_POINTER_DOWN || action == AMOTION_EVENT_ACTION_POINTER_UP) {
-			pointerIndex = (AMotionEvent_getAction(event) & AMOTION_EVENT_ACTION_POINTER_INDEX_MASK) >> AMOTION_EVENT_ACTION_POINTER_INDEX_SHIFT;
-			pointerId = AMotionEvent_getPointerId(event, pointerIndex);
-		}
-
-		engine->touchEventParamCache[0] = pointerId;
-		engine->touchEventParamCache[1] = action;
-		engine->touchEventParamCache[2] = AMotionEvent_getX(event, pointerIndex);
-		engine->touchEventParamCache[3] = AMotionEvent_getY(event, pointerIndex);
-		engine->touchEventParamCache[4] = engine->uptime.time;
-		engine->touchEventParamCache[5] = engine->uptime.millitm;
-		engine->touchEventParamCache[6] = AInputEvent_getDeviceId(event);
-		engine->touchEventParamCache[7] = AInputEvent_getSource(event);
-		
-		if (callSqFunction_Bool_Floats(engine->sqvm, "onMotionEvent", engine->touchEventParamCache, MOTION_EVENT_PARAMS_SIZE, false)) {
-			return 1;
-		}
-	}
-	return 0;
-}
-
-/*
- * Process key event
- */
-static int32_t emo_event_key(struct android_app* app, AInputEvent* event) {
-	struct engine* engine = (struct engine*)app->userData;
-
-	engine_update_uptime(engine);
-
-	engine->keyEventParamCache[0] = AKeyEvent_getAction(event);
-	engine->keyEventParamCache[1] = AKeyEvent_getKeyCode(event);
-	engine->keyEventParamCache[2] = AKeyEvent_getRepeatCount(event);
-	engine->keyEventParamCache[3] = AKeyEvent_getMetaState(event);
-	engine->keyEventParamCache[4] = engine->uptime.time;
-	engine->keyEventParamCache[5] = engine->uptime.millitm;
-	engine->keyEventParamCache[6] = AInputEvent_getDeviceId(event);
-	engine->keyEventParamCache[7] = AInputEvent_getSource(event);
-
-	if (callSqFunction_Bool_Floats(engine->sqvm, "onKeyEvent", engine->keyEventParamCache, KEY_EVENT_PARAMS_SIZE, false)) {
-		return 1;
-	}
-    return 0;
-}
-
-/**
- * Process the input event.
- */
-int32_t emo_handle_input(struct android_app* app, AInputEvent* event) {
-	if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION) {
-        return emo_event_motion(app, event);
-    } else if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_KEY) {
-        return emo_event_key(app, event);
-    }
-    return 0;
-}
-
-/*
- * handle sensor event TODO
- */
-static void engine_handle_sensors(struct engine* engine, ASensorEvent* event) {
-   LOGI("engine_handle_sensors: not implemented yet.");
-}
-
-/*
- * Gained focus
- */
-void emo_gained_focus(struct engine* engine) {
-	callSqFunction(engine->sqvm, "onGainedFocus");
-    engine->animating = 1;
-}
-
-/*
- * Lost focus
- */
-void emo_lost_focus(struct engine* engine) {
-	callSqFunction(engine->sqvm, "onLostFocus");
-    engine->animating = 0;
-}
-
-/**
- * Command from main thread: the system is running low on memory.
- * Try to reduce your memory use.
- */
-void emo_low_memory(struct engine* engine) {
-	callSqFunction(engine->sqvm, "onLowMemory");
-}
+extern void    emo_init_engine(struct engine* engine);
+extern void    emo_init_display(struct engine* engine);
+extern void    emo_draw_frame(struct engine* engine);
+extern void    emo_dispose_engine(struct engine* engine);
+extern int32_t emo_event_motion(struct android_app* app, AInputEvent* event);
+extern int32_t emo_event_key(struct android_app* app, AInputEvent* event);
+extern void    emo_event_sensors(struct engine* engine, ASensorEvent* event);
+extern void    emo_gained_focus(struct engine* engine);
+extern void    emo_lost_focus(struct engine* engine);
+extern void    emo_low_memory(struct engine* engine);
 
 /**
  * Initialize an EGL context for the current display.
@@ -265,15 +65,8 @@ static int engine_init_display(struct engine* engine) {
 
     eglInitialize(display, 0, 0);
 
-    /* Here, the application chooses the configuration it desires. In this
-     * sample, we have a very simplified selection process, where we pick
-     * the first EGLConfig that matches our criteria */
     eglChooseConfig(display, attribs, &config, 1, &numConfigs);
 
-    /* EGL_NATIVE_VISUAL_ID is an attribute of the EGLConfig that is
-     * guaranteed to be accepted by ANativeWindow_setBuffersGeometry().
-     * As soon as we picked a EGLConfig, we can safely reconfigure the
-     * ANativeWindow buffers to match, using EGL_NATIVE_VISUAL_ID. */
     eglGetConfigAttrib(display, config, EGL_NATIVE_VISUAL_ID, &format);
 
     ANativeWindow_setBuffersGeometry(engine->app->window, 0, 0, format);
@@ -330,6 +123,19 @@ static void engine_term_display(struct engine* engine) {
     engine->context = EGL_NO_CONTEXT;
     engine->surface = EGL_NO_SURFACE;
 }
+
+/**
+ * Process the input event.
+ */
+static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) {
+	if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION) {
+        return emo_event_motion(app, event);
+    } else if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_KEY) {
+        return emo_event_key(app, event);
+    }
+    return 0;
+}
+
 /**
  * Process the next main command.
  */
@@ -375,7 +181,7 @@ void android_main(struct android_app* state) {
     memset(&engine, 0, sizeof(engine));
     state->userData = &engine;
     state->onAppCmd = engine_handle_cmd;
-    state->onInputEvent = emo_handle_input;
+    state->onInputEvent = engine_handle_input;
     engine.app = state;
 
     if (state->savedState != NULL) {
@@ -392,9 +198,6 @@ void android_main(struct android_app* state) {
         int events;
         struct android_poll_source* source;
 
-        // If not animating, we will block forever waiting for events.
-        // If animating, we loop until all events are read, then continue
-        // to draw the next frame of animation.
         while ((ident=ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events,
                 (void**)&source)) >= 0) {
 
@@ -409,7 +212,7 @@ void android_main(struct android_app* state) {
                     ASensorEvent event;
                     while (ASensorEventQueue_getEvents(
                             engine.sensorEventQueue, &event, 1) > 0) {
-                        engine_handle_sensors(&engine, &event);
+                        emo_event_sensors(&engine, &event);
                     }
                 }
             }
