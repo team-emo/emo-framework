48640c8595b56c7ea4ec4a3b2aa56042c117da9e
Kota Iguchi <infosia.jp@gmail.com>
2011-03-26 06:52:32 +0000

    add namespace for emo classes

--- Emo-Android/jni/common.h
+++ Emo-Android/jni/common.h
@@ -6,9 +6,20 @@
 #define SQUIRREL_RUNTIME_SCRIPT "emoruntime.nut"
 #define SQUIRREL_MAIN_SCRIPT "emomain.nut"
 
-#define SQUIRREL_RUNTIME_CLASS "Runtime"
-#define SQUIRREL_EVENT_CLASS "Event"
-#define SQUIRREL_DRAWABLE_CLASS "Drawable"
+#define EMO_NAMESPACE  "emo"
+#define EMO_RUNTIME_CLASS  "Runtime"
+#define EMO_EVENT_CLASS    "Event"
+#define EMO_DRAWABLE_CLASS "Drawable"
+
+#define EMO_FUNC_ONLOAD         "emo::onLoad"
+#define EMO_FUNC_ONGAINED_FOUCS "emo::onGainedFocus"
+#define EMO_FUNC_ONLOST_FOCUS "  emo::onLostFocus"
+#define EMO_FUNC_ONDISPOSE      "emo::onDispose"
+#define EMO_FUNC_ONERROR        "emo::onError"
+#define EMO_FUNC_ONDRAW_FRAME   "emo::onDrawFrame"
+#define EMO_FUNC_ONLOW_MEMORY   "emo::onLowMemory"
+#define EMO_FUNC_MOTIONEVENT    "emo::_OnMotionEvent"
+#define EMO_FUNC_KEYEVENT       "emo::_OnKeyEvent"
 
 #define MOTION_EVENT_PARAMS_SIZE 8
 #define KEY_EVENT_PARAMS_SIZE    8
