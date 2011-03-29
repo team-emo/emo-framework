bfe7effe35fd038502e162b0094328f30a0f0b42
Kota Iguchi <infosia.jp@gmail.com>
2011-03-29 12:36:58 +0000

    add drawable code

---
+++ Emo-Android/jni/emo_drawable.h
@@ -0,0 +1,7 @@
+#include <squirrel.h>
+
+SQInteger emoDrawableCreate(HSQUIRRELVM v);
+SQInteger emoDrawableMove(HSQUIRRELVM v);
+SQInteger emoDrawableScale(HSQUIRRELVM v);
+SQInteger emoDrawableRotate(HSQUIRRELVM v);
+
