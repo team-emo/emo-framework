36f2af0b6b1a91a5f83c6ee7cacf6a659eb6bbbd
Kota Iguchi <infosia.jp@gmail.com>
2011-04-04 04:35:46 +0000

    add libpng

---
+++ Emo-Android/jni/libpng/Android.mk
@@ -0,0 +1,18 @@
+LIBPNG_SRC_FILES :=\
+	libpng/png.c \
+	libpng/pngerror.c \
+	libpng/pngget.c \
+	libpng/pngmem.c \
+	libpng/pngpread.c \
+	libpng/pngread.c \
+	libpng/pngrio.c \
+	libpng/pngrtran.c \
+	libpng/pngrutil.c \
+	libpng/pngset.c \
+	libpng/pngtrans.c \
+	libpng/pngwio.c \
+	libpng/pngwrite.c \
+	libpng/pngwtran.c \
+	libpng/pngwutil.c 
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/libpng
