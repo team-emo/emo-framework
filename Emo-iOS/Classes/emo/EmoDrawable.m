396c729128040f90972cf2e875448558a9ae97b6
Kota Iguchi <infosia.jp@gmail.com>
2011-04-12 09:28:27 +0000

    update glTexparameteri

--- Emo-iOS/Classes/emo/EmoDrawable.m
+++ Emo-iOS/Classes/emo/EmoDrawable.m
@@ -138,7 +138,7 @@ extern EmoEngine* engine;
 		glBindTexture(GL_TEXTURE_2D, texture.textureId);
 		
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
