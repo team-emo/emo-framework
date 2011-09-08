c910c2ece40a7ea1f9674bfc4ac56fcadb87f6c1
Kota Iguchi <infosia.jp@gmail.com>
2011-09-08 00:54:39 +0000

    add missing extern

--- Emo-iOS/libs/squirrel/squirrel/squtils.h
+++ Emo-iOS/libs/squirrel/squirrel/squtils.h
@@ -2,6 +2,10 @@
 #ifndef _SQUTILS_H_
 #define _SQUTILS_H_
 
+extern void *sq_vm_malloc(SQUnsignedInteger size);
+extern void *sq_vm_realloc(void *p,SQUnsignedInteger oldsize,SQUnsignedInteger size);
+extern void sq_vm_free(void *p,SQUnsignedInteger size);
+
 #define sq_new(__ptr,__type) {__ptr=(__type *)sq_vm_malloc(sizeof(__type));new (__ptr) __type;}
 #define sq_delete(__ptr,__type) {__ptr->~__type();sq_vm_free(__ptr,sizeof(__type));}
 #define SQ_MALLOC(__size) sq_vm_malloc((__size));
