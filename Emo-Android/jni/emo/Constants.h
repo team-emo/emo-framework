260b770b579efd8fd2a545ed0b6b0f89679ef1c0
Kota Iguchi <infosia.jp@gmail.com>
2011-11-29 21:55:05 +0000

    update build number

--- Emo-Android/jni/emo/Constants.h
+++ Emo-Android/jni/emo/Constants.h
@@ -28,7 +28,7 @@
 #ifndef EMO_CONSTANT_H
 #define EMO_CONSTANT_H
 
-#define EMO_BUILD_NUMBER 27
+#define EMO_BUILD_NUMBER 28
 
 // uncomment below to enable sandbox mode
 // #define EMO_WITH_SANDBOX
